e93669ed7489cc221aed70677c8499bc3ba77cc4
dictateurfou <stevenhervy@gmail.com>
2019-04-04 04:51:05 +0200

    little back

--- src/Service/VideoLinkValidator.php
+++ src/Service/VideoLinkValidator.php
@@ -83,7 +83,7 @@ class VideoLinkValidator
     {
         $result = false;
         $i = 0;
-        foreach (array_keys(self::AVAILABLEURL) as $key) {
+        foreach (self::AVAILABLEURL as $key => $value) {
             $e = 0;
             while (count(self::AVAILABLEURL[$key]) > $e) {
                 if (strpos($url, self::AVAILABLEURL[$key][$e])) {
